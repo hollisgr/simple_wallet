1dd7d02ae9559126c76564d10c700ceb72d532c8
hollisgr <hollisgr@student.21-school.ru>
2025-10-30 14:37:42 +0900

    dto fix

--- internal/dto/dto.go
+++ internal/dto/dto.go
@@ -2,16 +2,6 @@ package dto
 
 import "github.com/google/uuid"
 
-type WalletFromDB struct {
-	UUID    uuid.UUID `db:"uuid"`
-	Balance float64   `db:"balance"`
-}
-
-type WalletToWeb struct {
-	UUID    uuid.UUID `json:"valletId"`
-	Balance float64   `json:"balance"`
-}
-
 type WalletTransactionRequest struct {
 	UUID   uuid.UUID `json:"valletId" validate:"required,uuid"`
 	Type   string    `json:"operationType" validate:"required,oneof=DEPOSIT WITHDRAW"`
